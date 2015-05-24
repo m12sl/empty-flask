07959f14ab31eee270477fdfdb8ddae3bcc01dc3
m12sl <m12sl@yandex.ru>
2015-05-24 03:09:30 +0300

    add sample app

---
+++ hello.py
@@ -0,0 +1,12 @@
+__author__ = 'm12sl'
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello flask!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
